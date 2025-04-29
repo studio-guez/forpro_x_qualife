331ecdf18a95f79b0d609ddd376254037a44ba91
azertypow <nico@villa1203.ch>
2025-04-29 17:34:33 +0200

    feat: meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
     baseURL: '/',
     head: {
       title: 'Les rendez-vous des formateurs et formatrices',
+      meta: [
+        { name: 'description', content: 'Les rendez-vous des formateurs et formatrices d’apprenti·es sont des espaces collaboratifs pensés pour créer une communauté active de professionnel·les en charge des apprenti·es.' },
+      ],
       link: [
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
