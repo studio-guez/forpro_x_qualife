9d41a744689da672c417d9e6f0d91eb036c48c57
OctoY <yann@octoplus.solutions>
2026-04-15 16:58:27 +0200

    feat: add SVG and ICO favicon links in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,6 +47,8 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'canonical', href: 'https://rendezvousdesformateurs.ch' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
